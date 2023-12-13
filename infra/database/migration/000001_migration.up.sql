633a143803c2fcb61d22fdcfa54a4d43debdf6f4
arturbaccarin <arturbaccarin@github.com>
2023-12-13 17:13:19 -0300

    feat: add band table

--- infra/database/migration/000001_migration.up.sql
+++ infra/database/migration/000001_migration.up.sql
@@ -2,4 +2,10 @@ CREATE TABLE user (
     id INT PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
-)
+);
+
+CREATE TABLE band (
+    id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    year INT NOT NULL
+);
