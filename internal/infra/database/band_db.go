83ea8fa79078a98e5b961bfd29d1f1a93d1cf2dc
artur-baccarin <artur.baccarin@nappsolutions.com>
2023-12-20 11:24:18 -0300

    feat: add select and create band queries

---
+++ internal/infra/database/band_db.go
@@ -0,0 +1,53 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/bands-backend/internal/entity"
+)
+
+type Band struct {
+	DB *sql.DB
+}
+
+func NewBand(db *sql.DB) *Band {
+	return &Band{
+		DB: db,
+	}
+}
+
+func (b Band) SelectByID(ID int) (entity.Band, error) {
+	var band entity.Band
+
+	query := `
+		SELECT 
+			* 
+		FROM
+			band
+		WHERE
+			id = ?; 
+	`
+
+	row := b.DB.QueryRow(query, ID)
+
+	err := row.Scan(&band.ID, &band.Name, &band.Year)
+	if err != nil {
+		return band, err
+	}
+
+	return band, nil
+}
+
+func (b Band) Create(name string, year int) error {
+	query := `
+		INSERT INTO band (name, year) 
+		VALUES (?, ?);
+	`
+
+	_, err := b.DB.Exec(query, name, year)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
