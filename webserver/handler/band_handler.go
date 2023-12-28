459473f0271f3f4e32285d61f48236d6ad984388
arturbaccarin <arturbaccarin@github.com>
2023-12-28 11:24:45 -0300

    fix: add getbyid and crate

--- webserver/handler/band_handler.go
+++ webserver/handler/band_handler.go
@@ -7,10 +7,15 @@ import (
 	"github.com/arturbaccarin/band-backend/internal/dto"
 	"github.com/arturbaccarin/band-backend/internal/entity"
 	"github.com/arturbaccarin/band-backend/internal/infra/database"
+	"github.com/go-chi/chi"
 )
 
 type BandHandler struct {
 	BandDB database.BandInterface
+}
+
+type ErrorResponse struct {
+	Error string `json:"error"`
 }
 
 func NewBandHandler(bandDB database.BandInterface) *BandHandler {
@@ -25,20 +30,39 @@ func (h *BandHandler) Create(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&createBandParams)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(ErrorResponse{Error: err.Error()})
 		return
 	}
 
 	band, err := entity.NewBand(createBandParams.Name, createBandParams.Year)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(ErrorResponse{Error: err.Error()})
 		return
 	}
 
 	err = h.BandDB.Create(*band)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(ErrorResponse{Error: err.Error()})
 		return
 	}
 
 	w.WriteHeader(http.StatusCreated)
+}
+
+func (h *BandHandler) GetByID(w http.ResponseWriter, r *http.Request) {
+	ID := chi.URLParam(r, "ID")
+	band, err := h.BandDB.SelectByID(ID)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	if band.ID == 0 {
+		w.WriteHeader(http.StatusNoContent)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(band)
 }
