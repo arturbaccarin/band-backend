6f0e59448215629e59206baa25cda186310aa71d
arturbaccarin <arturbaccarin@github.com>
2023-12-28 11:24:27 -0300

    fix: change int to string

--- internal/infra/database/interface.go
+++ internal/infra/database/interface.go
@@ -4,5 +4,5 @@ import "github.com/arturbaccarin/band-backend/internal/entity"
 
 type BandInterface interface {
 	Create(entity.Band) error
-	SelectByID(uint) (entity.Band, error)
+	SelectByID(string) (entity.Band, error)
 }
