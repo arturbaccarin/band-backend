da24147e068dc3f374add80b9cf9272c1edbb72f
arturbaccarin <arturbaccarin@github.com>
2023-12-28 11:23:27 -0300

    fix: remover web server not used

--- config/config.go
+++ config/config.go
@@ -3,12 +3,11 @@ package config
 import "github.com/spf13/viper"
 
 type conf struct {
-	DBHost        string `mapstructure:"DB_HOST"`
-	DBPort        string `mapstructure:"DB_PORT"`
-	DBUser        string `mapstructure:"DB_USER"`
-	DBPassword    string `mapstructure:"DB_PASSWORD"`
-	DBName        string `mapstructure:"DB_NAME"`
-	WebServerPort string `mapstructure:"WEB_SERVER_PORT"`
+	DBHost     string `mapstructure:"DB_HOST"`
+	DBPort     string `mapstructure:"DB_PORT"`
+	DBUser     string `mapstructure:"DB_USER"`
+	DBPassword string `mapstructure:"DB_PASSWORD"`
+	DBName     string `mapstructure:"DB_NAME"`
 }
 
 func LoadConfig() *conf {
